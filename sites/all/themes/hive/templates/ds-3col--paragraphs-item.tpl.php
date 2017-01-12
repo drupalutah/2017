23300ae6d7e26ae003de959bfebf2a62846bde33
Jeremy Rasmussen <jeremy@jprasmussen.com>
2017-01-11 21:43:11 -0700

    paragraphs styles and templates home page styles misc style clean up

---
+++ sites/all/themes/hive/templates/ds-3col--paragraphs-item.tpl.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 3 column equal width template.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-3col-equal <?php print $classes;?> clearfix">
+<div class="inner">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <<?php print $left_wrapper ?> class="group-left<?php print $left_classes; ?>">
+    <?php print $left; ?>
+  </<?php print $left_wrapper ?>>
+
+  <<?php print $middle_wrapper ?> class="group-middle<?php print $middle_classes; ?>">
+    <?php print $middle; ?>
+  </<?php print $middle_wrapper ?>>
+
+  <<?php print $right_wrapper ?> class="group-right<?php print $right_classes; ?>">
+    <?php print $right; ?>
+  </<?php print $right_wrapper ?>>
+</div>
+</<?php print $layout_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
